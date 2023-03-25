318651776c59d798b543cacd6ca9eed9f3c0ca91
Michael Schori <michael@tie-international.com>
2023-03-25 10:21:07 +0100

    Added first todo-component and view

--- src/components/MyTodos.vue
+++ src/components/MyTodos.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    <h1>My Todos</h1>
   </div>
 </template>
 
 <script>
+import {onMounted, ref} from 'vue';
+import axios from 'axios';
+
 export default {
-  name: "MyTodos"
+  name: "MyTodos",
+
+  setup() {
+    const todos = ref([])
+
+    const getTodo = () => {
+      axios.get('http://127.0.0.1:8000/api/v1/todos')
+          .then(res => {
+            todos.value = res.data
+            console.log(todos.value);
+          })
+          .catch(err => {
+            console.log(err)
+          });
+    }
+
+    const addTodo = () => {
+
+    }
+
+    const toggleTodo = () => {
+
+    }
+
+    const removeTodo = () => {
+
+    }
+
+    onMounted(() => {
+      console.log('mounted')
+      getTodo()
+    })
+
+    return {
+      todos,
+      addTodo,
+      toggleTodo,
+      removeTodo
+    }
+  },
 }
 </script>
 
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>Todo View</h1>
+    <MyTodos/>
   </div>
 </template>
 
 <script>
+import MyTodos from "@/components/MyTodos.vue";
+
 export default {
-  name: "TodoView"
+  name: "TodoView",
+  components: {MyTodos}
 }
 </script>
 
